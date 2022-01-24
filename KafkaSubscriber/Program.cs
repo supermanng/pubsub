74c381ee5d7ddeb293d30c2dcc29d5e3d1c5c40e
Tolulope Awode <tolulope.awode@azenia.com>
2022-01-24 15:27:09 +0100

    Add project files.

---
+++ KafkaSubscriber/Program.cs
@@ -0,0 +1,59 @@
+﻿using Kafka.Public;
+using Kafka.Public.Loggers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LODLCMigration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            Console.Title = "Student Migration";
+            Console.WriteLine("Ongoing...");
+            CreateHostBuilder(args).Build().Run();
+        }
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+       Host.CreateDefaultBuilder(args)
+       .ConfigureServices((context, collection) =>
+       {
+           collection.AddHostedService<KafkaHostedServiceConsumer>();
+       });
+    }
+    public class KafkaHostedServiceConsumer : IHostedService
+    {
+        private readonly ILogger<KafkaHostedServiceConsumer> _logger;
+        private readonly ClusterClient _cluster;
+
+        public KafkaHostedServiceConsumer(ILogger<KafkaHostedServiceConsumer> logger)
+        {
+            _logger = logger;
+            _cluster = new ClusterClient(new Configuration
+            {
+                Seeds = "localhost:9092"
+            }, new ConsoleLogger());
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+
+            _cluster.ConsumeFromEarliest("testing");
+            _cluster.MessageReceived += record =>
+            {
+                var msg = $"{ Encoding.UTF8.GetString(record.Value as byte[]) }";
+                _logger.LogInformation($"Received: {msg} ");
+            };
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _cluster?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ TestKafka/Program.cs
@@ -0,0 +1,65 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LODLCMigration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Course Migration";
+            Console.Clear();
+            Console.WriteLine("Sending Courses...");
+            CreateHostBuilder(args).Build().Run();
+            //Console.WriteLine("Hello World!");
+
+        }
+
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, collection) =>
+            {
+                collection.AddHostedService<KafkaHostedServiceProducer>();
+            });
+    }
+    public class KafkaHostedServiceProducer : IHostedService
+    {
+        private readonly ILogger<KafkaHostedServiceProducer> _logger;
+        private readonly IProducer<Null, string> _producer;
+
+        public KafkaHostedServiceProducer(ILogger<KafkaHostedServiceProducer> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:9092"
+            };
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            for (var count = 0; count < 1000; count++)
+            {
+                var value = $"Total Student Migrated {count+ 1}";
+                _logger.LogInformation(value);
+                await _producer.ProduceAsync("testing", new Message<Null, string>()
+                {
+                    Value = value
+                }, cancellationToken);
+            }
+            _producer.Flush(TimeSpan.FromSeconds(12));
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _producer?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
